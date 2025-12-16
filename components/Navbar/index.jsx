e8bc06b75dcbc1844e815d912d557d9445efc267
akadedotun <adedotunayodimeji@gmail.com>
2025-12-16 23:22:40 +0000

    Update CV download to use Ayo-cv.pdf

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -15,7 +15,7 @@ const Navbar = ({color}) => {
         <div className="logo desk" onClick={() => linkTo('/')}  style={{color: color}} >Adedotun Ayodimeji <img src={ color == "black"? "/emoji.svg" : "/emoji-w.svg"} alt="" /> </div>
         <div className="logo mob"  onClick={() => linkTo('/')}  >AY <img src={ color == "black"? "/emoji.svg" : "/emoji-w.svg"} alt="" /> </div>
         {/* <h1 style={{color: "#fff"}} >{color}</h1> */}
-        <a href="/cv-dotun.pdf" target='_blank' download={true} >
+        <a href="/Ayo-cv.pdf?v=2" target='_blank' download="Ayo-cv.pdf" >
 
           <button className={`${inter.className}`}>Download CV</button> 
         </a>
