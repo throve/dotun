b0a25be53296b1a8073145e8107442e748851b9d
akadedotun <adedotunayodimeji@gmail.com>
2026-01-02 13:58:47 +0000

    Add line breaks to hero text, increase mobile font weight, add 12px padding to projects content on mobile

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className="hero" >
-        <p className="hero-intro">My name is Adedotun Ayodimeji, or AY. <span className="hero-location">I&apos;m a Product Designer in London,</span> designing experiences at TELUS Health.</p>
+        <p className="hero-intro">My name is Adedotun Ayodimeji, or AY.<br /> <span className="hero-location">I&apos;m a Product Designer in London,</span><br />designing experiences at TELUS Health.</p>
     </div>
   )
 }
